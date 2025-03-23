4c46803c1e7ad93d32b67d9de6ee763aa5526317
Király Sándor <kiralysandor1986@gmail.com>
2025-03-23 12:38:10 +0100

    - Redirect from index to app if logged in

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,8 +1,15 @@
 //this is landing page basically, this will be shown when the user is not logged in
 
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
+import { checkIfLoggedIn } from "./utils/auth";
 
 function Index() {
+    const navigate = useNavigate();
+    checkIfLoggedIn().then((isLoggedIn) => {
+        if (isLoggedIn) {
+            navigate("/app")
+        }
+    })
     return <>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/register">Register</NavLink>
